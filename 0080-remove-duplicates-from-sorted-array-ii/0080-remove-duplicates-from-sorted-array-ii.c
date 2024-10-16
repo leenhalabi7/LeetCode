53a76643848eb143f05cbb0ee07e7c16233da309
Leen Halabi <130834049+leenhalabi7@users.noreply.github.com>
2024-10-16 12:09:16 +0300

    Time: 13 ms (25.92%), Space: 11 MB (40.97%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.c
@@ -3,25 +3,29 @@ int removeDuplicates(int* nums, int numsSize) {
     int counter = 0, k = 1;
     for (int i = 1; i < numsSize; i++) 
     {
-        if (couple_on == false) //new num after non-dup num 
+        if (couple_on == false || nums[i] != nums[i - 1] ) //new num after non-dup num 
         {
             if (nums[i] == nums[i - 1])
             {
                 couple_on = true;
             }
+            else 
+            {
+                couple_on = false;
+            }
             nums[i - counter] = nums[i];
             k++;
         } 
-        else if (nums[i] == nums[i - 1]) //dup num
+        else //if (nums[i] == nums[i - 1]) //dup num
         {
             counter++;
         } 
-        else // new num after dup num 
+        /* else // new num after dup num 
         {
             nums[i - counter] = nums[i];
             couple_on = false;
             k++;
-        }
+        } */
     }
     return k;
 }
