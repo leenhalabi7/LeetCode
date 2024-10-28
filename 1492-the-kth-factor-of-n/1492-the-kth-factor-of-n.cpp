ab1860956bcf21376458272efb884078739cd30a
Leen Halabi <130834049+leenhalabi7@users.noreply.github.com>
2024-10-28 10:17:05 +0200

    Time: 0 ms (100.00%), Space: 7.7 MB (46.66%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+    
+    for( int i = n; i > 0; i--)
+    {
+        if( n%(i) == 0)
+        {
+            k--;
+            if( k == 0)
+            {
+                //return the kth factor
+                return n/i; 
+            } 
+        }
+    }
+    return -1; //if kth doesn't exist
+    }
+};
